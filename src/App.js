001719d33afaed634fd6d229d220689a31af08b9
tuong111 <32088321+tuong111@users.noreply.github.com>
2021-08-01 08:57:33 +0700

    Update App.js
    cmt this file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home_2 from './pages/home-2';
 function App() {
   return (
     <div className="App">
-      <Home_2></Home_2>
+//       <Home_2></Home_> //Add a coment this file
     </div>
   );
 }
