be24bd6d3298eb1cf0c96f4093f7c5ce78e90981
TuongNT <tuongtf@gmail.com>
2021-08-01 08:36:18 +0700

    add new h2

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,10 +1,11 @@
 import React from 'react'; 
 
-class Card extends Component {
+class Card extends React.Component{
     render() {
         return (
             <div>
                 <h1>This is my website</h1>
+                <h2>This is a h2</h2>
             </div>
         );
     }
